3779de6427239c6ce0e7470f7a41d1391505abfc
stangl <martoin.stangl@nksa.ch>
2020-12-15 10:55:56 +0100

    V2 car_game

--- x00_pygame/oop_first_steps/auto.py
+++ x00_pygame/oop_first_steps/auto.py
@@ -1,15 +1,17 @@
 import random
 
+import pygame
+
 
 class Auto:
-    def __init__(self, speed, tank, fahrbereit=True):
+    def __init__(self, pos, speed, tank, fahrbereit=True):
         self.color = (random.randint(0, 255),
                       random.randint(0, 255),
                       random.randint(0, 255))
         self.speed = speed
         self.tank = tank
         self.fahrbereit = fahrbereit
-        self.pos = [0, 0]
+        self.pos = pos
 
     def acc(self, a):
         self.speed += a
@@ -21,7 +23,13 @@ class Auto:
     def output(self):
         print("Farbe:",self.color,"Speed:",self.speed,"Tank:",self.tank)
 
+    def draw(self, win):
+        pygame.draw.rect(win, self.color, (self.pos[0], self.pos[1], 50, 20))
 
+    def auto_pilot(self):
+        self.pos[0] += self.speed
+        if self.pos[0] >= 700:
+            self.pos[0] = 0
 
 
     # def set_color(self,rw,gw,bw):
--- x00_pygame/oop_first_steps/main.py
+++ x00_pygame/oop_first_steps/main.py
@@ -6,26 +6,31 @@ pygame.init()
 
 width = 700
 height = 500
+
 win = pygame.display.set_mode((width, height))   # setup window
 pygame.display.set_caption("Car Sample")
 
 run = True
 
-auto1 = Auto(10, 100, False)
-auto2 = Auto(5, 80, False)
-auto1.speed = 10
-auto1.output()
-auto2.output()
+auto1 = Auto([250, 250],10, 70, False)
+auto2 = Auto([50, 50], 5, 80, False)
+auto3 = Auto([50, 100], 2, 80, False)
 
+auto1.speed = 10
+
+
+x = 30
+y = 50
 
 def check_keys():
+    global x, y
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT]:
-        pass
+    if keys[pygame.K_LEFT] and width > 0:
+        auto1.pos[0] -= 1
 
-    if keys[pygame.K_RIGHT]:
-        pass
+    if keys[pygame.K_RIGHT] and x < width-50:
+        auto1.pos[0] += 1
 
     if keys[pygame.K_UP]:
         pass
@@ -35,16 +40,20 @@ def check_keys():
 
 
 while run:
-    pygame.time.delay(50)
-
+    pygame.time.delay(10)
+    auto2.auto_pilot()
+    auto3.auto_pilot()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     check_keys()
-
     win.fill((0, 0, 0))  # Fills the screen with black
-    pygame.draw.rect(win, (255, 0, 0), (30, 30, 50, 20))   # https://www.pygame.org/docs/ref/draw.html
+    auto1.draw(win)
+    auto2.draw(win)
+    auto3.draw(win)
+
+    #pygame.draw.rect(win, (255, 0, 0), (x, y, 50, 20))   # https://www.pygame.org/docs/ref/draw.html
     pygame.display.update()
 
 pygame.quit()
