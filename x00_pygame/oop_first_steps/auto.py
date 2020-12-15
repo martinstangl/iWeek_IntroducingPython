b52ef350c0f73a3c10acb6339d1521ab760a0e32
stangl <martoin.stangl@nksa.ch>
2020-12-15 23:00:25 +0100

    car_game V3

--- x00_pygame/oop_first_steps/auto.py
+++ x00_pygame/oop_first_steps/auto.py
@@ -4,14 +4,20 @@ import pygame
 
 
 class Auto:
-    def __init__(self, pos, speed, tank, fahrbereit=True):
+    def __init__(self, pos, speed, tank, bot=True):
         self.color = (random.randint(0, 255),
                       random.randint(0, 255),
                       random.randint(0, 255))
         self.speed = speed
         self.tank = tank
-        self.fahrbereit = fahrbereit
         self.pos = pos
+        if bot:
+            self.img = pygame.transform.scale(
+                pygame.image.load("./images/car_red.png"), (40, 40))
+        else:
+            self.img = pygame.transform.scale(
+                pygame.image.load("./images/mario.png"), (40, 40))
+        self.rect = self.img.get_rect()   # get rect with position and size of image
 
     def acc(self, a):
         self.speed += a
@@ -24,15 +30,23 @@ class Auto:
         print("Farbe:",self.color,"Speed:",self.speed,"Tank:",self.tank)
 
     def draw(self, win):
-        pygame.draw.rect(win, self.color, (self.pos[0], self.pos[1], 50, 20))
+        #pygame.draw.rect(win, self.color, (self.pos[0], self.pos[1], 50, 20))
+        win.blit(self.img, (self.pos[0], self.pos[1]))
 
     def auto_pilot(self):
         self.pos[0] += self.speed
         if self.pos[0] >= 700:
             self.pos[0] = 0
+            self.pos[1] = random.randint(1, 500)
 
+    def check_col(self, cars):
+        for car in cars:
+            if self.img.get_rect().colliderect(car.img.get_rect()):
+                car.crashed()
+                self.crashed()
 
-    # def set_color(self,rw,gw,bw):
-    #     if 0 <= rw <= 255:
-    #         pass
+    def crashed(self):
+        self.speed = 0
 
+    def get_img_size(self):
+        print(self.img.get_rect().size)
--- x00_pygame/oop_first_steps/main.py
+++ x00_pygame/oop_first_steps/main.py
@@ -1,8 +1,14 @@
+import random
+
 from x00_pygame.oop_first_steps.auto import Auto
 
 import pygame   # https://www.pygame.org/
 
 pygame.init()
+
+life = 3
+heart_img = pygame.transform.scale(
+        pygame.image.load("./images/heart.png"), (40, 40))
 
 width = 700
 height = 500
@@ -12,48 +18,52 @@ pygame.display.set_caption("Car Sample")
 
 run = True
 
-auto1 = Auto([250, 250],10, 70, False)
-auto2 = Auto([50, 50], 5, 80, False)
-auto3 = Auto([50, 100], 2, 80, False)
+cars = []
+player = Auto([int(width/2), int(height/2)], 5, 100, False)
 
-auto1.speed = 10
+for i in range(10):     # autos erzeugen und in liste speichern
+    cars.append(Auto([random.randint(0, width),
+                     random.randint(0, height)],
+                     random.randint(1, 5), 70))
 
 
-x = 30
-y = 50
+
+def show_life():
+    for l in range(1, life+1):
+        win.blit(heart_img, (width-l*50, 10))
+
 
 def check_keys():
-    global x, y
     keys = pygame.key.get_pressed()
 
-    if keys[pygame.K_LEFT] and width > 0:
-        auto1.pos[0] -= 1
+    if keys[pygame.K_LEFT] and player.pos[0] > 0:
+        player.pos[0] -= 1
 
-    if keys[pygame.K_RIGHT] and x < width-50:
-        auto1.pos[0] += 1
+    if keys[pygame.K_RIGHT] and player.pos[0] < width:
+        player.pos[0] += 1
 
     if keys[pygame.K_UP]:
-        pass
+        player.pos[1] -= 1
 
     if keys[pygame.K_DOWN]:
-        pass
+        player.pos[1] += 1
 
 
 while run:
     pygame.time.delay(10)
-    auto2.auto_pilot()
-    auto3.auto_pilot()
+    win.fill((0, 0, 0))  # Fills the screen with black
+    for a in cars:
+        a.auto_pilot()
+        a.draw(win)
+        # a.check_col(cars)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
+    show_life()
     check_keys()
-    win.fill((0, 0, 0))  # Fills the screen with black
-    auto1.draw(win)
-    auto2.draw(win)
-    auto3.draw(win)
-
-    #pygame.draw.rect(win, (255, 0, 0), (x, y, 50, 20))   # https://www.pygame.org/docs/ref/draw.html
+    player.draw(win)
+   # player.get_img_size()
     pygame.display.update()
 
 pygame.quit()
